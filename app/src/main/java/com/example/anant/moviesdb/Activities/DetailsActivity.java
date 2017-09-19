3cc541f2675b652fd9d44d97608d9b470a18f4c1
Mohit Manuja <anantprsd5@gmail.com>
2017-09-20 00:00:36 +0530

    UI improvements

--- app/src/main/java/com/example/anant/moviesdb/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/anant/moviesdb/Activities/DetailsActivity.java
@@ -1,7 +1,10 @@
 package com.example.anant.moviesdb.Activities;
 
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.anant.moviesdb.R;
 
@@ -11,5 +14,10 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            ImageView imageView = (ImageView)findViewById(R.id.imageView2);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
     }
 }
